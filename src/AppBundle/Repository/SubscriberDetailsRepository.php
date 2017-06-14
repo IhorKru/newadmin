6ae4c7bbf8b1e5d326b9f521c71d44c640946195
IhorKru <kruchynenko@gmail.com>
2017-06-14 16:44:09 +0200

    Simplified creteria for camp selection

--- src/AppBundle/Repository/SubscriberDetailsRepository.php
+++ src/AppBundle/Repository/SubscriberDetailsRepository.php
@@ -9,13 +9,13 @@ class SubscriberDetailsRepository extends EntityRepository
     public function campEligibilityCalc($numcampaigns) {
         //$em = $this ->getDoctrine() ->getManager();
         ####selecting users that did not opt out from mailing resource
-        $qb0 = $this->getEntityManager()->createQueryBuilder();
+        /*$qb0 = $this->getEntityManager()->createQueryBuilder();
         $qb0
             -> select('opto')
             -> from('AppBundle\Entity\SubscriberOptOutDetails', 'opto')
             -> where('s.id = opto.user')
-        ;
-        ####selecting users that did not get email from us for 7 days + did not ever bounce hard + did not ever complaint
+        ;*/
+        ####selecting users that did not get email from us for 360 days + did not ever bounce hard + did not ever complaint
         $qb1 = $this->getEntityManager()->createQueryBuilder();
         $qb1
             ->select('send')
@@ -25,20 +25,20 @@ class SubscriberDetailsRepository extends EntityRepository
             ->andwhere('send.complaint <> 1')
             ->andwhere('s.emailaddress = send.emailaddress');
         ####selecting users that did not end up in ADK campaign error list
-        $qb2 = $this->getEntityManager()->createQueryBuilder();
+        /*$qb2 = $this->getEntityManager()->createQueryBuilder();
         $qb2
             -> select('adkerr')
             -> from('AppBundle\Entity\SubscriberADKCampErrors', 'adkerr')
             -> andwhere('s.emailaddress = adkerr.recipient')
-        ;
+        ;*/
         ####primary query to select users for future campaigns
         $qb = $this->getEntityManager()->createQueryBuilder();
         $qb
             -> select('s')
             -> from('AppBundle\Entity\SubscriberDetails', 's')
-            -> where($qb->expr()->not($qb->expr()->exists($qb0->getDQL())))
-            -> andWhere($qb->expr()->not($qb->expr()->exists($qb1->getDQL())))
-            -> andWhere($qb->expr()->not($qb->expr()->exists($qb2->getDQL())))
+            -> where($qb->expr()->not($qb->expr()->exists($qb1->getDQL())))
+            //-> andWhere($qb->expr()->not($qb->expr()->exists($qb0->getDQL())))
+            //-> andWhere($qb->expr()->not($qb->expr()->exists($qb2->getDQL())))
             -> setMaxResults($numcampaigns)
         ;
         $subscribers = $qb->getQuery()->getResult();
